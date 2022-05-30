cfbf10f2ef05e48372dccfa1f259204096e6cb66
ringotelyan <sute.dh9999@gmail.com>
2022-05-30 07:23:13 +0000

    [Update]トップページレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
